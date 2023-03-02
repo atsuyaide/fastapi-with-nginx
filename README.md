bdbb6d88f3e53a101e8829e9439b7b373ce89f8c
atsuyaide <kappa_deatsu@yahoo.co.jp>
2023-03-03 01:29:42 +0900

    update

---
+++ README.md
@@ -0,0 +1,43 @@
+# FastAPI サーバーを Nginx で経由で接続する
+
+## 役割
+
+- FastAPI: Web API サーバー
+- Nginx: リバースプロキシ
+
+## 構成
+
+- FastAPI
+  - `/users`: ユーザー ID のリストを返す
+  - `/docs`: Swagger UI
+  - `/redocs`: ReDoc UI
+- Nginx
+  - `/api/user-service/v1/users`: FastAPI サーバの`/users`にルーティング
+  - `/api/user-service/v1/docs`: FastAPI サーバの`/docs`にルーティング
+  - `/api/user-service/v1/redocs`: FastAPI サーバの`/redocs`にルーティング
+
+## 起動方法
+
+環境変数を設定
+
+```shell
+export $(cat ./webapp/env)
+```
+
+Nginx と FastAPI サーバを起動
+
+```shell
+docker compose up -d
+```
+
+## 接続
+
+ブラウザから Swagger UI に接続します.
+
+### Nginx 経由でアクセス
+
+`localhost:8080/api/user-service/v1/docs`にアクセス
+
+### FastAPI サーバに直接アクセス
+
+`localhost:8000/api/user-service/v1/docs`にアクセス
--- webapp/src/main.py
+++ webapp/src/main.py
@@ -1,12 +1,57 @@
-from fastapi import FastAPI, Request
+import logging
+import os
+from logging import getLogger
+
+from fastapi import APIRouter, FastAPI
+from fastapi.openapi.docs import (get_redoc_html, get_swagger_ui_html,
+                                  get_swagger_ui_oauth2_redirect_html)
+from fastapi.staticfiles import StaticFiles
+
+logging.basicConfig(level=logging.INFO)
+logger = getLogger(__name__)
+
+ENDPOINT_PREFIX = os.environ["ENDPOINT_PREFIX"]
+logger.info(ENDPOINT_PREFIX)
+
 
 app = FastAPI(
-    docs_url="/api/v1/docs",
-    redoc_url="/api/v1/redocs",
-    openapi_url="/api/v1/openapi.json",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=f"{ENDPOINT_PREFIX}/openapi.json",
 )
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+router = APIRouter()
 
 
-@app.get("/app")
-def read_main(request: Request):
-    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
+@router.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@router.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@router.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
+
+
+@router.get("/users")
+def read_main() -> list[int]:
+    return [1, 2, 3]
+
+
+app.include_router(router, prefix=ENDPOINT_PREFIX)
